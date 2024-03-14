76f774940c6d6361019a2df8b397820ffa744c88
User270298 <152844943+User270298@users.noreply.github.com>
2024-03-14 11:57:19 +0300

    end

--- Task_asyncio.py
+++ Task_asyncio.py
@@ -2,9 +2,8 @@ import asyncio
 import aiohttp
 import os
 import time
-import pickle
 import ast
-
+import aiofiles
 
 async def fetch(url):
     async with aiohttp.ClientSession() as session:
@@ -16,21 +15,23 @@ async def get_html(my_num):
     url1 = 'https://api.thecatapi.com/v1/images/search'
     url2 = 'https://random.dog/woof.json'
     tasks = []
-    for i in range(my_num):
-
+    for _ in range(my_num):
         tasks.append(fetch(url1))
         tasks.append(fetch(url2))
     res = await asyncio.gather(*tasks)
     count = 0
+    tasks.clear()
+
     for i in res:
         if my_num == count:
             break
         dictionary = ast.literal_eval(i)
         count += 1
         if isinstance(dictionary, list):
-            print(dictionary[0]['url'])
+            tasks.append(dictionary[0]['url'])
         else:
-            print(dictionary['url'])
+            tasks.append(dictionary['url'])
+    return tasks
 
 
 async def get_dog_or_cat(change):
@@ -43,38 +44,35 @@ async def get_dog_or_cat(change):
     for _ in range(count):
         task.append(fetch(url))
     res = await asyncio.gather(*task)
+    task.clear()
     for i in res:
         dictionary = ast.literal_eval(i)
         if change == '1':
-            print(dictionary['url'])
+            task.append(dictionary['url'])
         elif change == '2':
-            print(dictionary[0]['url'])
+            task.append(dictionary[0]['url'])
+    return task
 
 
-def save_images(images, pat):
-    with open(images, 'rb') as image:
-        file=image.read()
-    print(os.path(pat))
-    # with open(os.path(pat), 'bw') as f:
-    #     f.write(file)
-
-
+async def save_images(images):
+    for image in images:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image) as resp:
+                if resp.status == 200:
+                    f=await aiofiles.open(f'{image}'.rsplit('/', 1)[1], 'wb')
+                    await f.write(await resp.read())
+                    await f.close()
 
 def main():
     change = input('Change have dogs or cats or animals: '
                    '1-Dogs;\n2-Cats;\n3-Animals\n')
     if change == '1':
-        f=asyncio.run(get_dog_or_cat(change))
-        save_images(f, 'DZ_60_asyncio')
+        f = asyncio.run(get_dog_or_cat(change))
     elif change == '2':
-        asyncio.run(get_dog_or_cat(change))
+        f = asyncio.run(get_dog_or_cat(change))
     elif change == '3':
-        asyncio.run(get_html(int(input('Enter count imagines: '))))
+        f = asyncio.run(get_html(int(input('Enter count imagines: '))))
+    asyncio.run(save_images(f))
+
 
 main()
-# results= await asyncio.gather(*tasks)
-# for result in results:
-#     print(result)
-
-
-# asyncio.run(get_html())
